11613ee1c24b5b5de43a28f127cdf83b16eb2c99
i.kurilov <kurilov>
2018-03-28 18:23:39 +0300

    - v1.0.11 : fixed export of interfaces;

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,3 +1,5 @@
 export {SelectDictModule} from './select-dict.module';
 export {SelectDictComponent} from './select-dict.component';
 export {SelectDictService} from './select-dict.service';
+export {ISelectDictService} from './select-dict.service';
+export {IDictListContainer} from './select-dict.component'
